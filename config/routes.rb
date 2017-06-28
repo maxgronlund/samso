5b298f634962a0d962629d72a028c060869bf11e
Max Grønlund <max@synthmax.dk>
2017-06-29 01:03:19 +0200

    cleaning up

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
 
     get '/:locale' => 'home#index'
     root to: "home#index"
-    resources :users
+    resources :users, except: [:index]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
