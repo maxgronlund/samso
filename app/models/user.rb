3b4c672f0c8a9aca65348585172e0a84fb3d3df3
Max Grønlund <max@synthmax.dk>
2017-06-28 21:14:41 +0200

    image validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ApplicationRecord
   }
 
   # Validate the attached image is image/jpg, image/png, etc
-  validates_attachment_content_type :avatar, content_type: %r{ /\Aimage\/.*\Z/ }
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
   def super_admin?
     roles.where(permission: Role::SUPER_ADMIN).any?
