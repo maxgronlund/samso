50263828f67205c3cad0137a03fa76bc5d738180
Max Grønlund <max@synthmax.dk>
2017-06-28 19:08:02 +0200

    amazon upload on localhost

--- Gemfile
+++ Gemfile
@@ -40,6 +40,8 @@ gem 'glyphicons-rails'
 gem 'jquery-rails'
 gem 'slim-rails'
 gem 'simple_form'
+gem 'paperclip'
+gem 'aws-sdk', '~> 2.3'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,16 @@ class User < ApplicationRecord
 
   accepts_nested_attributes_for :roles
 
+  has_attached_file :avatar, styles: {
+      thumb: '100x100>',
+      square: '200x200#',
+      medium: '300x300>'
+    }
+
+  # Validate the attached image is image/jpg, image/png, etc
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
+
   def super_admin?
     roles.where(permission: Role::SUPER_ADMIN).any?
   end
