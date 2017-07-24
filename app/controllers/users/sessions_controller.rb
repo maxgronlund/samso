6e3f8cd9b2003933579ad054f6d17c335e86c785
Max Grønlund <max@synthmax.dk>
2017-07-24 11:30:24 +0200

    restored user_session_controller

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,7 +2,6 @@ class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
   def after_sign_in_path_for(resource)
-    session.delete :page_id
     if session[:new_payment_path]
       session[:after_sign_in_path] = session[:new_payment_path]
       session.delete :new_payment_path
