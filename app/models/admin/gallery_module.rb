382888f3de6b70e842706cf2b052d90ef29bcfc9
Max Grønlund <max@synthmax.dk>
2017-07-31 08:23:53 +0200

    updating gallery_module

--- app/models/admin/gallery_module.rb
+++ app/models/admin/gallery_module.rb
@@ -3,10 +3,18 @@ class Admin::GalleryModule < ApplicationRecord
   include SectionPlugin
   has_many :images, class_name: 'Admin::GalleryImage', dependent: :destroy, foreign_key: 'gallery_module_id'
 
+  #scope :latest_images, ->  { order('created_at DESC') }
+  # scope :latest_images, -> { order('created_at DESC') }
+
   def page_module
     PageModule.find_by(
       moduleable_type: 'Admin::GalleryModule',
       moduleable_id: id
     )
   end
+
+  def latest_images
+    images.order('created_at DESC')
+  end
+
 end
