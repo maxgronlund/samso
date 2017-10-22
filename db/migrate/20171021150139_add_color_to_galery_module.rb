ad3b1756bb6a5e9739838629eb00b1ee0e1bafa8
Max Grønlund <max@synthmax.dk>
2017-10-22 11:04:01 +0200

    removed migration

--- db/migrate/20171021150139_add_color_to_galery_module.rb
+++
@@ -1,9 +0,0 @@
-# fix
-class AddColorToGaleryModule < ActiveRecord::Migration[5.1]
-  def change
-    add_column :admin_gallery_modules, :color, :string, default: '#000000'
-    add_column :admin_gallery_modules, :background_color, :string, default: '#FFFFFF'
-    add_column :admin_image_modules, :color, :string, default: '#000000'
-    add_column :admin_image_modules, :background_color, :string, default: '#FFFFFF'
-  end
-end
