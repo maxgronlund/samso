b66fe305742df7533eb2872da32fc9d0ae264354
Max Grønlund <max@synthmax.dk>
2018-05-13 09:49:38 +0200

    still authorize epaper

--- app/controllers/api/v1/epaper_verification_controller.rb
+++ app/controllers/api/v1/epaper_verification_controller.rb
@@ -6,24 +6,28 @@ class Api::V1::EpaperVerificationController < ApplicationController
   private
 
   def access_to_epaper?
+
     params.permit!
     Rails.logger.debug '-----------key------------'
     Rails.logger.debug params[:secret]
 
     epaper_token = EPaperToken.find_by(secret: params[:secret])
 
-    Rails.logger.debug '-----------epaper_token------------'
-    Rails.logger.debug epaper_token
-    return false if epaper_token.nil?
-    Rails.logger.debug '-------------unused----------'
+    Rails.logger.debug '-----------epaper_token.nil?------------'
+    Rails.logger.debug epaper_token.nil?
+    #return false if epaper_token.nil?
+    Rails.logger.debug '-------------unused?----------'
     Rails.logger.debug epaper_token.unused?
-    return false unless epaper_token.unused?
-    Rails.logger.debug '-------------user----------'
-    Rails.logger.debug epaper_token.user
-    user = epaper_token.user
-    return false if user.nil?
+    # return false unless epaper_token.unused?
+    Rails.logger.debug '-------------user.nil?----------'
+    Rails.logger.debug epaper_token.user.nil?
+    user = epaper_token.user.nil?
+    #return false if user.nil?
     Rails.logger.debug '-------------access_to_epaper----------'
-    user.access_to_epaper?
+    Rails.logger.debug user.access_to_epaper?
+    true
+  rescue
+    Rails.logger.debug '-------------rescued----------'
     true
   end
 end
