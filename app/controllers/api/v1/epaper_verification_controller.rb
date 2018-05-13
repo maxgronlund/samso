abfe5c4e05310db916610ff3353474f3898a4457
Max Grønlund <max@synthmax.dk>
2018-05-13 10:25:44 +0200

    still authorize epaper

--- app/controllers/api/v1/epaper_verification_controller.rb
+++ app/controllers/api/v1/epaper_verification_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::EpaperVerificationController < ApplicationController
   def index
-    @access_to_epaper = access_to_epaper?
+    @access_to_epaper = true#access_to_epaper?
   end
 
   private
