1537ad75fdbe8d72aa2ddb553ef3a25a10854fc2
Max Grønlund <max@synthmax.dk>
2018-05-13 09:40:06 +0200

    still authorize epaper

--- app/controllers/api/v1/epaper_verification_controller.rb
+++ app/controllers/api/v1/epaper_verification_controller.rb
@@ -7,18 +7,23 @@ class Api::V1::EpaperVerificationController < ApplicationController
 
   def access_to_epaper?
     params.permit!
-    Rails.logger.debug '-----------string------------'
-    Rails.logger.debug params["secret"]
     Rails.logger.debug '-----------key------------'
     Rails.logger.debug params[:secret]
 
     epaper_token = EPaperToken.find_by(secret: params[:secret])
+
+    Rails.logger.debug '-----------epaper_token------------'
     Rails.logger.debug epaper_token
-    Rails.logger.debug '-----------------------'
     return false if epaper_token.nil?
+    Rails.logger.debug '-------------unused----------'
+    Rails.logger.debug epaper_token.unused?
     return false unless epaper_token.unused?
+    Rails.logger.debug '-------------user----------'
+    Rails.logger.debug epaper_token.user
     user = epaper_token.user
     return false if user.nil?
+    Rails.logger.debug '-------------access_to_epaper----------'
     user.access_to_epaper?
+    true
   end
 end
