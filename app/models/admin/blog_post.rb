7b42003a4ce87c9ba801f489467e1baa81838bf9
Max Grønlund <max@synthmax.dk>
2018-10-28 18:44:36 +0100

    RAILS_HOST environment variable

--- app/models/admin/blog_post.rb
+++ app/models/admin/blog_post.rb
@@ -1,16 +1,10 @@
 # Post in the blog
 class Admin::BlogPost < ApplicationRecord
-  # include PgSearch
-
   include Elasticsearch::Model
   include Elasticsearch::Model::Callbacks
 
   index_name    "articles-#{Rails.env}"
   document_type "post"
-
-
-
-
 
   settings index: { number_of_shards: 1 } do
     mappings dynamic: 'false' do
@@ -19,17 +13,6 @@ class Admin::BlogPost < ApplicationRecord
       indexes :signature, analyzer: 'danish', type: 'text'
     end
   end
-
-  # settings do
-  #   mappings dynamic: false do
-  #     indexes :title, type: :text
-  #     indexes :subtitle, type: :text, analyzer: :danish
-  #     indexes :teaser, type: :text, analyzer: :danish
-  #     indexes :body, type: :text, analyzer: :danish
-  #     # indexes :published, type: :boolean
-  #   end
-  # end
-
 
   attr_accessor :delete_image, :page_id
   belongs_to :blog, class_name: 'Admin::Blog', counter_cache: true, optional: true
@@ -40,25 +23,6 @@ class Admin::BlogPost < ApplicationRecord
     large: '770x770>',
     full_size: '1110x1110>'
   }
-
-  # pg_search_scope :search_by_content, against: %i[title body subtitle teaser signature]
-  # multisearchable against: %i[title body subtitle teaser signature]
-  # pg_search_scope :search_by_title_or_body, against: %i[title body subtitle teaser signature]
-
-  # pg_search_scope(
-  #   :search_by_title_or_body,
-  #   against: %i(
-  #     title
-  #     body
-  #     subtitle
-  #     signature
-  #   ),
-  #   using: {
-  #     tsearch: {
-  #       dictionary: "english",
-  #     }
-  #   }
-  # )
 
   validates :body, presence: true unless Rails.env == 'test'
   validates_attachment_content_type :image, content_type: %r{\Aimage\/.*\Z}
@@ -71,23 +35,6 @@ class Admin::BlogPost < ApplicationRecord
       [I18n.t('blog_post.image_left'), 'image_left'],
       [I18n.t('blog_post.image_right'), 'image_right']
     ].freeze
-
-  # def self.search_published(query)
-  #   __elasticsearch__.search({
-  #     query: {
-  #       bool: {
-  #         must: [
-  #           {
-  #             multi_match: {
-  #               query: query,
-  #               fields: [:title, :body, :signature]
-  #             }
-  #           }
-  #         ]
-  #       }
-  #     }
-  #   })
-  # end
 
   def self.elasticsearch(query)
     __elasticsearch__.search(
@@ -110,23 +57,6 @@ class Admin::BlogPost < ApplicationRecord
       }
     )
   end
-
-  # def self.demo_search(query)
-  #   response = self.search(
-  #       query:     { match:  { title: query } },
-  #       highlight: {
-  #         pre_tags: ['<em class="label-highlight">'],
-  #         post_tags: ['</em>'],
-  #         fields: {
-  #           title:   { number_of_fragments: 0 },
-  #           body: { fragment_size: 25 }
-  #         }
-  #       }
-  #     )
-  #   response.results#.first.highlight.title
-  # end
-
-  # h({query: {match: { title: "asdf" }}, sort: [title: {order:'desc'}]})
 
   def show_on_page
     Page.find_by(id: post_page_id) || Page.find_by(locale: I18n.locale)
