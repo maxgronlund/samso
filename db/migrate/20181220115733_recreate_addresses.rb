aa713c33ecae558541d6223868c7c2531beff2eb
Max Grønlund <max@synthmax.dk>
2018-12-20 13:14:05 +0100

    fixing addresses migration

--- db/migrate/20181220115733_recreate_addresses.rb
+++ db/migrate/20181220115733_recreate_addresses.rb
@@ -7,8 +7,16 @@ def up
       t.string :address
       t.integer :zipp_code
       t.string :city
+      t.string :address_type, default: "primary_address"
+      t.date :start_date
+      t.date :end_date
 
       t.timestamps
     end
+  end
+
+  def down
+    drop_table :addresses
+    rename_table :addresses_old, :addresses
   end
 end
