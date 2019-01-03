eb818e343e6177109df5d4bf764648abf9983f89
Max Grønlund <max@synthmax.dk>
2019-01-04 00:25:42 +0100

    login IP on users/show

--- app/models/user.rb
+++ app/models/user.rb
@@ -147,8 +147,7 @@ class User < ApplicationRecord
 
   def subscription_id
     return last_valid_subscription.subscription_id if last_valid_subscription.present?
-
-    legacy_subscription_id.presence || ''
+    ''
   end
 
   def expired_subscriber?
