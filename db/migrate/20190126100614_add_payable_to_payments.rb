1275ac8e62769649261c8e57b4d3726e1d6885ca
Max Grønlund <max@synthmax.dk>
2019-01-26 14:41:18 +0100

    fixed migration

--- db/migrate/20190126100614_add_payable_to_payments.rb
+++ db/migrate/20190126100614_add_payable_to_payments.rb
@@ -1,6 +1,6 @@
 class AddPayableToPayments < ActiveRecord::Migration[5.2]
   def change
-    add_reference :payments, :payable_id, :integer
-    add_reference :payments, :payable_type, :string
+    add_column :payments, :payable_id, :integer
+    add_column :payments, :payable_type, :string
   end
 end
