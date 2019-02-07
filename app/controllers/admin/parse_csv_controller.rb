252e78c34864329f50efb7f0f9925c372ffc6216
Max Grønlund <maxgronlund@Maxs-MacBook-Pro.local>
2019-02-07 15:24:45 +0100

    bug fix

--- app/controllers/admin/parse_csv_controller.rb
+++ app/controllers/admin/parse_csv_controller.rb
@@ -4,8 +4,8 @@
 class Admin::ParseCsvController < AdminController
   before_action :no_editor, only: %i[index show edit update destroy]
   def new
-    @admin_csv_import.update(imported: Time.zone.now)
     @admin_csv_import = Admin::CsvImport.find(params[:csv_import_id])
+    @admin_csv_import.update(imported: Time.zone.now)
     import_service.import(@admin_csv_import)
     redirect_to @admin_csv_import
   end
