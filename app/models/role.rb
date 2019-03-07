e123c28743c88fb563f845e2a075010242123b12
maxgronlund <max@synthmax.dk>
2019-03-07 20:40:42 +0100

    translation on admin create user and roles

--- app/models/role.rb
+++ app/models/role.rb
@@ -7,4 +7,10 @@ class Role < ApplicationRecord
   SUPER_ADMIN = 'super_admin'
   ROLES = [ADMIN, MEMBER, EDITOR]
   belongs_to :user
+
+  TRANSLATED_ROLES = [
+    [I18n.t('role.' + MEMBER), MEMBER],
+    [I18n.t('role.' + ADMIN), ADMIN],
+    [I18n.t('role.' + EDITOR), EDITOR]
+  ]
 end
