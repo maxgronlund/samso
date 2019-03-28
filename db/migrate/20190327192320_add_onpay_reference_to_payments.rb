ff7de1c8c90e9c72c9db6d289247dcdb2d3a05b5
maxgronlund <max@synthmax.dk>
2019-03-28 13:44:34 +0100

    default value on payments.onpay_referince

--- db/migrate/20190327192320_add_onpay_reference_to_payments.rb
+++ db/migrate/20190327192320_add_onpay_reference_to_payments.rb
@@ -1,5 +1,5 @@
 class AddOnpayReferenceToPayments < ActiveRecord::Migration[5.2]
   def change
-    add_column :payments, :onpay_reference, :string
+    add_column :payments, :onpay_reference, :string, default: 'na'
   end
 end
