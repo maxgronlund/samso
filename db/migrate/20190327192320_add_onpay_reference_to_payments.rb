d08602beea7786f75d791ae37453ebf441763cae
maxgronlund <max@synthmax.dk>
2019-03-28 13:57:47 +0100

    outcomment update script from migration

--- db/migrate/20190327192320_add_onpay_reference_to_payments.rb
+++ db/migrate/20190327192320_add_onpay_reference_to_payments.rb
@@ -2,10 +2,10 @@ class AddOnpayReferenceToPayments < ActiveRecord::Migration[5.2]
   def change
     add_column :payments, :onpay_reference, :string, default: 'na'
 
-    Payment.find_each do |payment|
-      if payment.uuid.present? && payment.onpay_reference.blank?
-        payment.update(onpay_reference: payment.uuid)
-      end
-    end
+    # Payment.find_each do |payment|
+    #   if payment.uuid.present? && payment.onpay_reference.blank?
+    #     payment.update(onpay_reference: payment.uuid)
+    #   end
+    # end
   end
 end
