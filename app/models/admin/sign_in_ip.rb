4229ef857711e1dc18a0c001fbcaca76c7de08fb
maxgronlund <max@synthmax.dk>
2019-05-26 01:14:31 +0200

    ip lodation lookup on SignInIp

--- app/models/admin/sign_in_ip.rb
+++ app/models/admin/sign_in_ip.rb
@@ -1,3 +1,21 @@
+require 'net/http'
+
 class Admin::SignInIp < ApplicationRecord
   belongs_to :user, counter_cache: true
+
+  def location
+    # curl -u 9bd1d2726a3a47 ipinfo.io/152.115.55.39
+    url = URI.parse("http://ipinfo.io/#{ip.to_s}?token=9bd1d2726a3a47")
+    req = Net::HTTP::Get.new(url.to_s)
+
+    res = Net::HTTP.start(url.host, url.port) {|http|
+      http.request(req)
+    }
+    JSON.parse(res.body)
+  end
 end
+
+
+
+
+
