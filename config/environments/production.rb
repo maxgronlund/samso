5191ff68ec56138a1ffb39651ddb3dc36364f5d7
Max Grønlund <maxgronlund@Maxs-2019-Mac.local>
2019-10-28 09:15:21 +0100

    downgraded elasticsearch from 7 -> 6

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -119,6 +119,5 @@ Rails.application.configure do
   }
 
   config.secret_key_base = ENV.fetch('SECRET_KEY_BASE')
-
   Elasticsearch::Model.client = Elasticsearch::Client.new host: ENV['SEARCHBOX_SSL_URL']
 end
