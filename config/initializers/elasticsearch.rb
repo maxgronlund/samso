5f37ea6eca68d7d54ee4dcbf695b0266ea9602d3
Max Grønlund <maxgronlund@bigmax.local>
2020-04-06 13:19:32 +0200

    outcommented content in searchbox donfig

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,8 +1,8 @@
-if Rails.env.production?
-  Searchkick.client =
-    Elasticsearch::Client.new(
-      hosts: ["https://paas:a8a01d233d3291abe7f0721b9f909663@gloin-eu-west-1.searchly.com"],
-      retry_on_failure: true,
-      transport_options: {request: {timeout: 250}}
-    )
-end
+# if Rails.env.production?
+#   Searchkick.client =
+#     Elasticsearch::Client.new(
+#       hosts: ["https://paas:a8a01d233d3291abe7f0721b9f909663@gloin-eu-west-1.searchly.com"],
+#       retry_on_failure: true,
+#       transport_options: {request: {timeout: 250}}
+#     )
+# end
