bfc8190829d1dbc331a313f49384dcf5fb217d9b
Max Grønlund <maxgronlund@bigmax.local>
2020-09-11 20:07:06 +0200

    updated rails eo 6.0.3.2

--- features/step_definitions/blog_post_content_steps.rb
+++ features/step_definitions/blog_post_content_steps.rb
@@ -1,15 +1,12 @@
-Then("I visit an article page") do
-
+Then('I visit an article page') do
   blog = FactoryBot.create(:blog)
 
-  blog_module =
-    FactoryBot
-    .create(
-      :blog_module,
-      admin_blog_id: blog.id,
-      show_search: false,
-      show_all_categories: true
-    )
+  FactoryBot.create(
+    :blog_module,
+    admin_blog_id: blog.id,
+    show_search: false,
+    show_all_categories: true
+  )
 
   blog_post =
     FactoryBot
@@ -23,18 +20,13 @@ Then("I visit an article page") do
   visit admin_blog_blog_post_path(I18n.locale, blog_post.blog, blog_post)
 end
 
-Then("I fill the blog_post_content_form and submit it") do
+Then('I fill the blog_post_content_form and submit it') do
   content = persisted_fake_blog_post_content_form_data
-
-  # ask('does that look right?')
   fill_in 'admin_blog_post_content_image_caption', with: content[:image_caption]
-  ask('does that look right?')
   click_on I18n.t('save')
 end
 
-Then("I can see the blog_post_content i created") do
-
+Then('I can see the blog_post_content i created') do
   content = persisted_fake_blog_post_content_form_data
   expect(page).to have_content(content[:image_caption])
 end
-
